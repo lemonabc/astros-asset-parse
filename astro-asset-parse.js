75380d1a6868b39951069c3c756a4b78dbd4d792
tiger <wanhu88@gmail.com>
2016-06-07 15:24:36 +0800

    bug fix

--- astro-asset-parse.js
+++ astro-asset-parse.js
@@ -44,7 +44,8 @@ module.exports = new astro.Middleware('*', function(asset, next) {
         info = info || {};
         if (!info.modType) {
             info.modType = 'static';
-            info.path = nodePath.join(prjCfg.root, 'assets', url)
+            info.name = url;
+            info.filePath = nodePath.join(prjCfg.root, 'assets', url)
             info.fileType = nodePath.extname(url);
             if(info.fileType && info.fileType.charAt(0) == '.'){
                 info.fileType = info.fileType.substr(1)
